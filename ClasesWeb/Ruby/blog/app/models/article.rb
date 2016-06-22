7207e129f5138145d0151f15224e4dccfdaacff9
galarchenko <cristoph.galarchenko@gmail.com>
2016-06-22 12:03:34 -0300

    avances en validaciones

--- ClasesWeb/Ruby/blog/app/models/article.rb
+++ ClasesWeb/Ruby/blog/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	validates :title, {presence: {message: "El título no puede estar en blanco"}, uniqueness: true}
+	validates :body, {presence: {message: "El cuerpo no puede estar en blanco"}, length: {minimum: 20, message: "El cuerpo no puede ser menos a 20"}}
 end
