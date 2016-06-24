29332450db15520eb9aa4a96b1082ace8fbfa718
galarchenko <cristoph.galarchenko@gmail.com>
2016-06-24 17:14:46 -0300

    avances en autenticacion y validaciones

--- ClasesWeb/Ruby/blog/app/controllers/articles_controller.rb
+++ ClasesWeb/Ruby/blog/app/controllers/articles_controller.rb
@@ -2,11 +2,13 @@ class ArticlesController < ApplicationController
 	
 	#GET /articles
 	def index
+		# Obtiene todos los articles
 		@articles = Article.all
 	end
 
 	#GET /articles/:id
 	def show
+		# Encuentra un article por id
 		@article = Article.find(params[:id])
 	end
 
@@ -17,12 +19,38 @@ class ArticlesController < ApplicationController
 
 	#POST /articles
 	def create
-		@article = Article.new({title: params[:article][:title], body: params[:article][:body]})
+		@article = Article.new(article_params)
 
 		if @article.save
 			redirect_to @article
 		else
 			render :new
 		end
+	end
+
+	def update
+		# @article.update_attributes({title: 'Nuevo titulo'})
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit				
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy #Elimina el obj de la db
+		redirect_to articles_path
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 end
